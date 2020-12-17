fcc90a4907c87071ac6eaa0187a3b25ebec4661b
Brigai-dev <71457538+Brigai-dev@users.noreply.github.com>
2020-12-17 12:36:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def student():
-   return render_template('student.html')
+   return render_template('input.html')
 
 
 @app.route('/result',methods = ['POST', 'GET'])

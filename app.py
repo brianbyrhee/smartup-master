e706e4252b3c730cb6f09e29175198d132d100c6
Brian Sleeva <briguy3917@gmail.com>
2020-12-21 12:13:33 -0500

    Commit after final project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import parser as fl
 app = Flask(__name__)
 @app.route('/')
 def student():
@@ -9,9 +10,16 @@ def student():
 def result():
    if request.method == 'POST':
       thing = request.form
+      # a
       result = {}
       for key, value in thing.items():
-          result[key] = value + "hi"
+         ret_val = fl.Parser(value)
+      avg_cost, avg_success, expected_cat = ret_val.get_values()
+      # store avg cost and avg success in a dictionary
+      result = {}
+      result["Expected Category"] = expected_cat
+      result["Average Project Goal For Category (USD)"] = avg_cost
+      result["Chance of Success based on prior projects(%)"] = avg_success
       return render_template("result.html",result = result)
 
 
--- parser.py
+++ parser.py
@@ -1,244 +1,164 @@
 import re
 from gensim.summarization import keywords
 import collections
-import csv
 import heapq
 import gensim.downloader as gdl
 import ssl
 import nltk
 import pymysql
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize 
+from nltk.tokenize import word_tokenize
 from scipy import spatial
-import gensim.downloader as gdl
-
+import numpy as np
+ 
 ssl._create_default_https_context = ssl._create_unverified_context
-
+ 
 class Parser:
-  def __init__(self, data, input_text = "", most_similar = 3):
-
-    self.input_text = input_text
-    self.dataset = data
-    self.most_similar = most_similar
-    self.currency_dict = {'AUD': 0.76,
-                'CAD': 0.79,
-                'CHF': 1.13,
-                'DKK': 0.16,
-                'EUR': 1.23,
-                'GBP': 1.36,
-                'HKD': 0.13,
-                'JPY': 0.0097,
-                'MXN': 0.05,
-                'NOK': 0.12,
-                'NZD': 0.72,
-                'SEK': 0.12,
-                'SGD': 0.75,
-                'USD': 1.0}
-    self.glove = gdl.load("glove-wiki-gigaword-100")
-    self.combined_data = self.combine_data()
-    self.category_dict, self.mcategory_dict = self.get_categories()
-    self.top_keywords = self.get_keywords()
-    self.token_dict, self.costs_dict, self.success_dict = self.get_dictionary()
-    print("token dict:", self.token_dict)
-    print(self.top_keywords)
-    self.max_category = self.find_category()
-    print(self.max_category, self.return_values())
-
-
-    # create connection
-    connection = pymysql.connect(host='localhost',
-                                user='root',
-                                password='ferhat437',
-                                db='mydatabase')
-
-    # Create cursor
-    my_cursor = connection.cursor()
-
-    # define the category
-    main_category = "Publishing" 
-
-    # query for finding all categories and storing them in a list
-
-    sqlquery = "SELECT DISTINCT main_category from projects"
-
-    my_cursor.execute(sqlquery)
-    result = my_cursor.fetchall()
-
-    maincategory_lst = []
-
-    for i in result:
-        maincategory_lst.append(i[0])
-
-    print(maincategory_lst)
-
-    # query to find count of projects of main category
-    sqlquery = "SELECT COUNT(id) from projects WHERE main_category = '{}'".format(main_category)
-    my_cursor.execute(sqlquery)
-    count = my_cursor.fetchall()
-
-    total_count = count[0][0]
-
-
-
-    # query to find count of successful projects of main category
-    sqlquery = "SELECT COUNT(id) from projects WHERE main_category = '{}' AND (state = 'successful' OR state = 'live')".format(main_category)
-    my_cursor.execute(sqlquery)
-    success_count = my_cursor.fetchall()
-
-    total_successes = success_count[0][0]
-
-
-    # query to find avg goal amount of main category
-    sqlquery = "SELECT AVG(usd_goal_real) from projects WHERE main_category = '{}'".format(main_category)
-    my_cursor.execute(sqlquery)
-    result = my_cursor.fetchall()
-
-    avg = result[0][0]
-
-
-    # Close the connection
-    connection.close()
-    
-
-
-  def open_data(self, data_csv):
-    #returns the data in a 2d list
-    data_list = []
-    with open(data_csv, 'r', encoding="utf8", errors='ignore') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        row = row[:12]
-        if row[0] != 'ID':
-          data_list.append(row)
-      return data_list
-    
-  def combine_data(self):
-    #combined multiple data into one datastream
-    combined_data = []
-    for data in self.dataset:
-        combined_data.append(self.open_data(data))
-    combined_data = [item for sublist in combined_data for item in sublist]
-    return combined_data
+ def __init__(self, input_text = "", most_similar = 3):
+ 
+   self.input_text = input_text
+   self.most_similar = most_similar
+   self.currency_dict = {'AUD': 0.76,
+               'CAD': 0.79,
+               'CHF': 1.13,
+               'DKK': 0.16,
+               'EUR': 1.23,
+               'GBP': 1.36,
+               'HKD': 0.13,
+               'JPY': 0.0097,
+               'MXN': 0.05,
+               'NOK': 0.12,
+               'NZD': 0.72,
+               'SEK': 0.12,
+               'SGD': 0.75,
+               'USD': 1.0}
+   self.categories =['Publishing', 'Film & Video','Music','Food','Design','Games','Fashion','Theater','Comics','Crafts','Art','Photography','Technology','Dance','Journalism']
+   self.glove = self.create_glove("glove.6B.100d.txt")
+   self.top_keywords = self.get_keywords()
+   print("Top keywords:", self.top_keywords)
+   #self.token_dict, self.costs_dict, self.success_dict = self.get_dictionary()
+   self.max_category = self.find_category()
+   self.total_count, self.total_successes, self.avg_cost = self.parse_query()
+ 
+ 
+ def parse_query(self):
+   # create connection
+   connection = pymysql.connect(host='localhost',
+                               user='root',
+                               password='ferhat437',
+                               db='mydatabase')
+ 
+   # Create cursor
+   my_cursor = connection.cursor()
+ 
+   # define the category
+   main_category = self.max_category
+ 
+   # query for finding all categories and storing them in a list
+ 
+   sqlquery = "SELECT DISTINCT main_category from projects"
+ 
+   my_cursor.execute(sqlquery)
+   result = my_cursor.fetchall()
+ 
+   maincategory_lst = []
+ 
+   for i in result:
+       maincategory_lst.append(i[0])
+ 
+   # query to find count of projects of main category
+   sqlquery = "SELECT COUNT(id) from projects WHERE main_category = '{}'".format(main_category)
+   my_cursor.execute(sqlquery)
+   count = my_cursor.fetchall()
+ 
+   total_count = count[0][0]
+ 
+   # query to find count of successful projects of main category
+   sqlquery = "SELECT COUNT(id) from projects WHERE main_category = '{}' AND (state = 'successful' OR state = 'live')".format(main_category)
+   my_cursor.execute(sqlquery)
+   success_count = my_cursor.fetchall()
+ 
+   total_successes = success_count[0][0]
+ 
+ 
+   # query to find avg goal amount of main category
+   sqlquery = "SELECT AVG(usd_goal_real) from projects WHERE main_category = '{}'".format(main_category)
+   my_cursor.execute(sqlquery)
+   result = my_cursor.fetchall()
+ 
+   avg = result[0][0]
+ 
+   # Close the connection
+   connection.close()
+   return total_count, total_successes, avg
   
-  def get_categories(self):
-    categories = collections.Counter([x[2] for x in self.combined_data])
-    categories = {key: val for key, val in categories.items() if val > 5}
-    main_categories = collections.Counter([y[3] for y in self.combined_data])
-    main_categories = {key: val for key, val in main_categories.items() if val > 5}
-    #print("categories:", categories)
-    #print("main categories:", main_categories)
+ def get_values(self):
+   return round(self.avg_cost, 2), round(self.total_successes / self.total_count, 3), self.max_category
+ 
+ def create_glove(self, gloveFile):
+   with open(gloveFile, encoding="utf8" ) as f:
+       content = f.readlines()
+   model = {}
+   for line in content:
+       splitLine = line.split()
+       word = splitLine[0]
+       embedding = np.array([float(val) for val in splitLine[1:]])
+       model[word] = embedding
+   return model
+ 
+ def get_keywords(self):
+   print(self.input_text)
+   text_keywords = keywords(self.input_text, words = 5, scores = True, split = True, lemmatize = True)
+   keywords_f = []
+   for t in text_keywords:
+     words = t[0].split(" ")
+     for word in words:
+       keywords_f.append((word, t[1]))
+   return keywords_f
+ 
+ def get_similarity(self, project, token_list, k=1):
+   print("project:", project)
+   print(token_list)
+   maximum_heap = []
+   for category in token_list:
+       total_sim = 0
+       for token in category:
+           for data in project:
+               token_vec = self.glove[token.lower()]
+               data_vec = self.glove[data[0]]
+               similarity = 1 - spatial.distance.cosine(data_vec, token_vec)
+               similarity *= data[1]
+               total_sim += similarity
+       total_sim /= len(category)
+       if len(project) > 0:
+         total_sim /= len(project)
+       maximum_heap.append((-total_sim, category))
+      
+   heapq.heapify(maximum_heap)
+          
+   top_k = maximum_heap[:k]
   
-    return categories, main_categories
+   return [(-score, token) for score, token in top_k]
+ 
+ def find_category(self):
+   y_tokens = [self.clean_sentence(c) for c in self.categories]
+   topic_cat = self.get_similarity(self.top_keywords, y_tokens)[0]
+   print("max category in topic form:", topic_cat)
+   for i in range(len(y_tokens)):
+       if topic_cat[1][0] == y_tokens[i][0]:
+           max_cat = self.categories[i]
+           return max_cat
+ 
+ def clean_sentence(self, s):
+   #makes sentence readable for similarity analysis
+   s = s.replace(r'-', ' ')
+   s = s.split(" ")
+   s = [re.sub(r'[^\w\s]', '', char) for char in s]
+   s = [x for x in s if x != '']
+   return s
+ 
+ 
+# dummy = Parser("garden house")
+# print(dummy.top_keywords)
+# print(dummy.get_values())
 
-  def get_keywords(self):
-    print(self.input_text)
-    text_keywords = keywords(self.input_text, words = 5, scores = True, split = True, lemmatize = True)
-    keywords_f = []
-    for t in text_keywords:
-      words = t[0].split(" ")
-      for word in words:
-        keywords_f.append((word, t[1]))
-    return keywords_f
-
-  def get_dictionary(self):
-    stop_words = set(stopwords.words('english'))  
-
-    x = [d[1] for d in self.combined_data[1:]]
-    y = [d[3] for d in self.combined_data[1:]]
-    success = [d[9] for d in self.combined_data[1:]]
-    cost = [d[6] for d in self.combined_data[1:]]
-    currency = [d[4] for d in self.combined_data[1:]]
-
-    token_dictionary = collections.defaultdict(list)
-    costs = collections.defaultdict(int)
-    success_rate = collections.defaultdict(int)
-
-    for index in range(len(x)):
-        title = x[index]
-        cleaned = self.clean_sentence(title)
-        cleaned_str = ""
-        if not cleaned:
-            continue
-        for word in cleaned:
-            cleaned_str += word + " "
-        word_tokens = word_tokenize(cleaned_str)  
-
-        filtered_sentence = [w for w in word_tokens if not w in stop_words]  
-
-        filtered_sentence = []  
-
-        for w in word_tokens:  
-            if w not in stop_words:  
-                filtered_sentence.append(w)  
-        
-        x_token = []
-        print("glove:", self.glove)
-        if len(filtered_sentence) >= 3:
-            for token in filtered_sentence:
-                if len(x_token) == 3:
-                    break
-                if token in self.glove:
-                    x_token.append(token)
-        print("x_token:", x_token)
-        if x_token:
-            topic = y[index]
-            did_succeed = success[index]
-            if did_succeed == 'live' or did_succeed == 'successful':
-                success_rate[topic] += 1
-                costs[topic] += float(cost[index]) * self.currency_dict[currency[index]]
-            token_dictionary[topic].append(x_token)
-        return token_dictionary, costs, success_rate
-
-  def get_similarity(self, project, token_list, k=1):
-    maximum_heap = []
-    for category in token_list:
-        total_sim = 0
-        for token in category:
-            for data in project:
-                token_vec = self.glove[token]
-                data_vec = self.glove[data[0]]
-                similarity = 1 - spatial.distance.cosine(data_vec, token_vec)
-                similarity *= data[1]
-                total_sim += similarity
-        total_sim /= len(project) * len(category)
-        maximum_heap.append((-total_sim, category))
-        
-    heapq.heapify(maximum_heap)
-            
-    top_k = maximum_heap[:k]
-    
-    return [(-score, token) for score, token in top_k]
-
-  def find_category(self):
-    categories = list(self.token_dict.keys())
-    y_tokens = [self.clean_sentence(c) for c in categories]
-    print(self.top_keywords, y_tokens)
-    topic_cat = self.get_similarity(self.top_keywords, y_tokens)
-    for i in range(len(y_tokens)):
-        if topic_cat[1][0] == y_tokens[i][0]:
-            max_cat = categories[i]
-            return max_cat
-
-  def return_values(self):
-    max_cost = self.costs_dict[self.max_category]
-    max_length = len(self.token_dict[self.max_category])
-    max_success = self.success_dict[self.max_category]
-    print(max_cost, max_length, max_success)
-    avg_cost = max_cost / max_length
-    avg_success = max_success / max_length
-    return avg_cost, avg_success
-
-  def clean_sentence(self, s):
-    #makes sentence readable for similarity analysis
-    s = s.replace(r'-', ' ')
-    s = s.split(" ")
-    s = [re.sub(r'[^\w\s]', ' ', char) for char in s]
-    s = [x for x in s if x != '']
-    return s
-
-
-text_en = (
-    'STUDIO IN THE SKY - A Documentary Feature Film (Canceled)')
-
-test = Parser(['ks-projects-201612.csv', 'ks-projects-201801.csv'], text_en)
-#print(test.get_categories())
